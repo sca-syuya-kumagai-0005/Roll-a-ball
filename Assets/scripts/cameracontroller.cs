00d70419681f689489faf797c782205911a6a0ad
熊谷　柊哉 <sdt22csc0005@edu.sca.ac.jp>
2023-05-12 17:19:05 +0900

    ゲームアップロード

---
+++ Assets/scripts/cameracontroller.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameracontroller : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+    private Vector3 offset;
+    public static float roty;
+    public static Quaternion camerarot;
+    [SerializeField]
+    private GameObject camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position=WallManager.startpositon();
+        roty = transform.rotation.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+        offset = transform.position - player.transform.position;
+        if (Input.GetKey(KeyCode.Z))
+        {
+            roty-=0.4f;
+            transform.Rotate(new Vector3(0, roty + offset.y,0 )*Time.deltaTime);
+        }
+        else if(Input.GetKey(KeyCode.X))
+        {
+            roty+=0.4f;
+            transform.Rotate(new Vector3(0, roty + offset.y, 0) * Time.deltaTime);
+        }
+        else
+        {
+            roty=0;
+        }
+    }
+
+  public static Quaternion  CameraRotate()
+    {
+        return camerarot;
+    }
+}
---
+++ Assets/scripts/playercontroller.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class playercontroller : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    private Rigidbody rb;
+    private int score = 0;
+    [SerializeField]
+    private Text scoretext;
+    [SerializeField]
+    private GameObject player;
+    Vector3 StartPositon;
+    Vector3  y;
+    Vector3 playerposition;
+
+    private bool flag;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb=GetComponent<Rigidbody>();  
+        Debug.Log(player.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!flag)
+        {
+            player.transform.position = WallManager.startpositon();
+            playerposition = player.transform.position;
+            flag=true;
+        }
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+        var movement = new Vector3(moveHorizontal, 0, moveVertical);
+        rb.AddForce(movement * speed * Time.deltaTime);
+        if (player.transform.position.y <= 0)
+        {
+            player.transform.position.y= 1.0f;
+        }
+        //var ‚ÍŒ^„˜_@‰E•Ó‚©‚çŒ^‚ðŽ©“®‚Å„‘ª‚µ‚Ä‚­‚ê‚é
+
+        scoretext.text="Count:"+score.ToString();
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        
+        if(other.gameObject.CompareTag("Item.pickup"))
+        {
+            other.gameObject.SetActive(false);
+            score+=1;
+        }
+        if(score>=5)
+        {
+            SceneManager.LoadScene("Clear");
+        }
+    }
+}
